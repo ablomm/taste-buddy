0d66f6c4d1f35f80d77ecbb381c958e1cd0f4fdf
sabuazma <sabuazma@uwo.ca>
2023-11-15 20:52:13 -0500

    TB-47: getting the login page started

--- frontend/App.js
+++ frontend/App.js
@@ -5,7 +5,6 @@ import LoginPage from "./pages/LoginPage";
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <LoginPage/>
       <StatusBar style="auto" />
     </SafeAreaView>
@@ -15,8 +14,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
