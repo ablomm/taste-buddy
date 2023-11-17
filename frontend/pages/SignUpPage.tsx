c9ba051cc1316bfdb92244ca455d22b9f4e1f90b
ivyd <iivy.dding@gmail.com>
2023-11-17 12:15:40 -0500

    Adding front-end validation to sign up form (incomplete)

--- frontend/pages/SignUpPage.tsx
+++ frontend/pages/SignUpPage.tsx
@@ -1,15 +1,59 @@
 import React from 'react';
 import {View, Text, TextInput, StyleSheet, Button, Pressable} from "react-native";
+import * as yup from 'yup';
+import {useForm, Controller} from 'react-hook-form';
+import {yupResolver} from '@hookform/resolvers/yup';
 
 const SignUpPage = () =>{
+
+// define validation rules for each field
+const schema = yup.object().shape({
+    username: yup
+    .string()
+    .required('Username is required')
+    .min(3, 'Username must contain at least 3 characters'),
+    password: yup
+    .string()
+    .required('Password is required')
+    .min(10, 'Password must contain at least 10 characters'),
+    confirmPassword: yup
+    .string()
+    .required('Confirm password is required')
+    .oneOf([yup.ref('password'),''], "Passwords don't match")
+
+});
+
+//set up form with validation schema, pass resolver property with yupResolver(schema) to handle validation logic
+const{
+    control,
+    handleSubmit,
+    formState: {errors},
+} = useForm({
+    resolver: yupResolver(schema),
+    defaultValues:{
+        username: ''
+    }
+})
 
 const [username, onChangeUsername] = React.useState('');
 const [email, onChangeEmail] = React.useState('');
 const [password, onChangePassword] = React.useState('');
 const [passwordConfirm, onChangePasswordConfirm] = React.useState('');
 
-
     return(<View>
+        <Controller 
+        control={control}
+        rules={{required:true,}}
+        render={({field:{onChange, value}}) => (
+            <TextInput 
+            style={styles.input}
+            value ={value}
+            onChangeText={onChange}
+            />
+        )}
+        name="username"
+        />
+         {errors.username && <Text>{errors.username.message}</Text>}
         <TextInput style={styles.input}
         onChangeText={onChangeUsername}
         value={username}
