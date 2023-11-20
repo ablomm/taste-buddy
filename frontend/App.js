0e2912429fba1b20cc840d48190308bbf8c325a6
sabuazma <sabuazma@uwo.ca>
2023-11-20 11:01:47 -0500

    Tb-47: textinputs incomplete

--- frontend/App.js
+++ frontend/App.js
@@ -1,5 +1,7 @@
+import {userState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native';
+import {Stack, userRouter} from 'expo-router';
 import LoginPage from "./pages/LoginPage";
 
 export default function App() {
