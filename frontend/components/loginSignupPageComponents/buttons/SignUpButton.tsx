a74ad142b0c4a2d88e14ed58f46074ab29bd4937
sabuazma <sabuazma@uwo.ca>
2023-11-22 21:23:56 -0500

    navigation login and signup

--- frontend/components/loginSignupPageComponents/buttons/SignUpButton.tsx
+++ frontend/components/loginSignupPageComponents/buttons/SignUpButton.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import {View, Text, StyleSheet, TextInput, Button, Pressable} from "react-native";
 import styles from '../../../styles/loginSignupPageComponents/buttons/loginSignupButtonStyles';
 
-const SignUpButton = () =>{
+const SignUpButton = ({handlePress}:any) =>{
     return(
         <View style={styles.container}>
-            <Pressable style={({pressed}) => [{backgroundColor: pressed ? '#BDBDBD' : 'white',} ,styles.button]}>
+            <Pressable style={({pressed}) => [{backgroundColor: pressed ? '#BDBDBD' : 'white',} ,styles.button]} onPress={handlePress}>
                 <Text style={styles.buttonText}>Sign Up</Text>
             </Pressable>
         </View>
