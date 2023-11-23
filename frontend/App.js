6ae8b571515a01d91f8b92db0afa8d2f1164d0c9
talmoha@uwo.ca <talmoha@uwo.ca>
2023-11-22 23:47:36 -0500

    remove from main paige

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import LoginPage from "./pages/LoginPage";
-import DietaryPreference from './pages/DietaryPreference';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <DietaryPreference />
+      <Text>Open up App.js to start working on your app!</Text>
+      <LoginPage />
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
