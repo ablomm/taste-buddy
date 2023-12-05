5696ecd092142ca73f99553a9020569b8d5c11a5
HerbertShin <hshin63@uwo.ca>
2023-12-05 15:33:33 -0500

    TB-55: Logout functionality

--- frontend/src/screens/AccountPage.tsx
+++ frontend/src/screens/AccountPage.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import LogoutButton from "../components/loginSignupPageComponents/buttons/LogoutButton";
+import {UserContext} from "../providers/UserProvider";
 
 const AccountPage = () => {
+    const userContext = React.useContext(UserContext) as any;
+
     return (
         <View>
             <Text>
                 Account Page
             </Text>
+            <LogoutButton handlePress={userContext.logout} isButtonInteractable={true}/>
         </View>
     );
 }
