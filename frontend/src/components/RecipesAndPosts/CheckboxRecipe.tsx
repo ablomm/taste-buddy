d7dbf5b36f4484c57bcc88f015e9164362c834d5
sabuazma <sabuazma@uwo.ca>
2024-01-09 11:27:17 -0500

    TB-41: checkboxes

--- frontend/src/components/RecipesAndPosts/CheckboxRecipe.tsx
+++ frontend/src/components/RecipesAndPosts/CheckboxRecipe.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import { View, StyleSheet,} from "react-native";
+import React, {useState} from 'react';
+import { View, Text, StyleSheet,} from "react-native";
 import Checkbox from 'expo-checkbox';
 
-const CheckboxRecipe = () => {
+const CheckboxRecipe = ({checkboxText}:any) => {
+    const [isChecked, setChecked] = useState(false);
     return(
         <View style={styles.container}>
             <Checkbox
-                value={false}
-                onValueChange={setSelection}
+                value={isChecked}
+                onValueChange={setChecked}
                 style={styles.checkbox}
+                color={"#00D387"}
             />
+            <Text style={styles.checkboxText}>{checkboxText}</Text>
         </View>
     );
 }
 const styles = StyleSheet.create({
     container: {
         flexDirection:'row',
-        justifyContent: 'space-between',
         margin: 5,
         alignItems: 'center'
     },
+    checkbox: {
+        margin: 5,
+        marginRight: 10,
+    },
+    checkboxText: {
+        fontSize: 15,
+        fontWeight: "500"
+    }
 });
 export default CheckboxRecipe;
