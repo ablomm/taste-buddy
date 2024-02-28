ddc6e9815d8c1b6238f54cd6d1bfe9e0be161d24
Ivy <iivy.dding@gmail.com>
2024-02-28 17:11:33 -0500

    updated swipe functionality with partial implementation of saving/rejecting recipes
    - new swipe functionality implemented
    - linked to display recipes from database
    - partial implementation of fetching new list when user swipes through all recipes (not complete)
    - partial implementation of saving/rejecting recipes

--- frontend/src/screens/RecommenderPage.tsx
+++ frontend/src/screens/RecommenderPage.tsx
@@ -1,235 +1,201 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, Image, ImageSourcePropType, Button, StyleSheet, Pressable } from 'react-native';
-import Animated, { Extrapolate, interpolate, FadeInDown, useSharedValue } from 'react-native-reanimated';
-import Carousel from "react-native-reanimated-carousel";
-import { TAnimationStyle } from '../components/RecommenderPage/BaseLayout';
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, ImageBackground, Alert } from "react-native";
 import { window } from "../constants";
-import { PanGesture } from 'react-native-gesture-handler';
-import { TouchableRipple } from 'react-native-paper';
-import TBButton from '../components/TBButton';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import TBButton from "../components/TBButton";
+import { TinderCard } from "rn-tinder-card";
+import { LinearGradient } from "expo-linear-gradient";
+import { UserContext } from "../providers/UserProvider";
 
 const RecommenderPage = () => {
+  const userContext = React.useContext(UserContext) as any;
+  const username = userContext.state.username;
+  const PAGE_WIDTH = window.width;
+  const PAGE_HEIGHT = window.height;
 
-    const PAGE_WIDTH = window.width;
-    const PAGE_HEIGHT = 0.75 * window.height;
-    const directionAnimVal = useSharedValue(0);
+  const [recipes, setRecipes] = useState<any | null>(null); //recipes to display to user
+  const [loading, setLoading] = useState(false); //to display loading signal
+  const [displayRecipeNumber, setDisplayRecipeNumber] = useState(0); //for how many recipes have been shown to determine refresh
 
-    const dataLength = 10;
-    const data = [1, 1, 1];
-    const [recipes, setRecipes] = useState();
-
-    const tempRecipes = [{ "id": 1, "authorID": 1, "creationTime": "2024-01-11T21:30:29.475Z", "recipeTitle": "Cajun Honey Glazed Chicken ", "description": "This cajun honey glazed chicken is a delicious dinner idea! The chicken is actually cooked IN the marinade, allowing for maximum flavour.", "cookTimeHours": 0, "cootTimeMinutes": 30, "calories": 0, "servings": 4, "recipeImage": "https://tastebuddy-images.s3.us-east-2.amazonaws.com/af534c1081afa0a031b760eeac9a825a" }, { "id": 2, "authorID": 1, "creationTime": "2024-01-11T21:30:53.964Z", "recipeTitle": "Cajun Honey Glazed Chicken ", "description": "This cajun honey glazed chicken is a delicious dinner idea! The chicken is actually cooked IN the marinade, allowing for maximum flavour.", "cookTimeHours": 0, "cootTimeMinutes": 30, "calories": 0, "servings": 4, "recipeImage": "https://tastebuddy-images.s3.us-east-2.amazonaws.com/02383e9bc2297516e2eb341691e71539" }, { "id": 3, "authorID": 1, "creationTime": "2024-01-12T01:05:50.087Z", "recipeTitle": "testing title", "description": "", "cookTimeHours": 0, "cootTimeMinutes": 0, "calories": 0, "servings": 0, "recipeImage": "https://tastebuddy-images.s3.us-east-2.amazonaws.com/a839793446691e48363db4aa6f8ccc40" }, { "id": 4, "authorID": 1, "creationTime": "2024-01-12T01:06:16.800Z", "recipeTitle": "testing recipe title 2", "description": "", "cookTimeHours": 0, "cootTimeMinutes": 0, "calories": 0, "servings": 0, "recipeImage": "https://tastebuddy-images.s3.us-east-2.amazonaws.com/1066f99fc7a99d17388a7ceea9e5898c" }]
-
-    const fetchRecipeList = async () => {
-        try {
-            const response = await fetch(`${process.env.EXPO_PUBLIC_SERVER_URL || "http://localhost:8080"}/recipe/get-all-recipes`, {
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                credentials: 'include',
-            });
-
-            await response.json().then(result => {
-                setRecipes(result)
-            });
-        } catch (error) {
-            console.error(error);
+  const fetchRecipeList = async () => {
+    try {
+      const response = await fetch(
+        `${
+          process.env.EXPO_PUBLIC_SERVER_URL || "http://localhost:8080"
+        }/recipe/get-all-recipes`,
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
         }
+      );
+
+      await response.json().then((result) => {
+        // setRecipes([...recipes, result]);
+        setRecipes(result);
+        setDisplayRecipeNumber(result.length - 1);
+      });
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    useEffect(() => {
-        fetchRecipeList();
-    }, []);
+  const updateRecipeList = async () => {
+    try {
+      const response = await fetch(
+        `${
+          process.env.EXPO_PUBLIC_SERVER_URL || "http://localhost:8080"
+        }/recipe/get-all-recipes`,
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        }
+      );
 
-    const fixedDirection = "negative";
-    const onConfigurePanGesture = (g: PanGesture) => {
-        g.onChange(e => {
-            directionAnimVal.value = Math.sign(e.translationX)
-        })
-    };
+      await response.json().then((result) => {
+        // setRecipes([...recipes, result]);
+        setRecipes(result);
+        setDisplayRecipeNumber(result.length - 1);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const animationStyle: TAnimationStyle = React.useCallback(
-        (value: number) => {
-            "worklet";
-            const translateY = interpolate(
-                value,
-                [0, 1],
-                [0, -18],
-            );
+  const rejectRecipe = async (recipeID: number) => {
+    setDisplayRecipeNumber(displayRecipeNumber - 1);
+  };
 
-            const translateX = interpolate(
-                value,
-                [-1, 0],
-                [PAGE_WIDTH, 0],
-                Extrapolate.CLAMP
-            ) * directionAnimVal.value;
+  const saveRecipe = async (recipeID: number, username: any) => {
+    try {
+      // const;
+      setDisplayRecipeNumber(displayRecipeNumber - 1);
+    } catch (error) {
+      console.error(error);
+    }
+    checkRecipeList();
+  };
 
-            const rotateZ = interpolate(
-                value,
-                [-1, 0],
-                [15, 0],
-                Extrapolate.CLAMP
-            ) * directionAnimVal.value;
+  const checkRecipeList = async () => {
+    if (displayRecipeNumber == 1) {
+      console.log("test append");
+      updateRecipeList();
+    }
+  };
 
-            const zIndex = interpolate(
-                value,
-                [0, 1, 2, 3, 4],
-                [0, 1, 2, 3, 4].map((v) => (data.length - v) * 10),
-                Extrapolate.CLAMP
-            )
+  useEffect(() => {
+    fetchRecipeList();
+  }, []);
 
-            const scale = interpolate(
-                value,
-                [0, 1],
-                [1, 0.95]
-            );
-
-            const opacity = interpolate(
-                value,
-                [-1, -0.8, 0, 1],
-                [0, 0.9, 1, 0.85],
-                Extrapolate.EXTEND
-            );
-
-            return {
-                transform: [
-                    { translateY },
-                    { translateX },
-                    { rotateZ: `${rotateZ}deg` },
-                    { scale },
-                ],
-                zIndex,
-                opacity,
-            };
-        },
-        [PAGE_HEIGHT, PAGE_WIDTH],
-    );
-
-    // const carouselProps = {
-    //     loop: false,
-    //     style:{
-    //         width: PAGE_WIDTH,
-    //         height: PAGE_HEIGHT,
-    //         justifyContent: "center",
-    //         alignItems: "center",
-    //         backgroundColor: "white",
-    //     },
-    //     defaultIndex:0,
-    //     vertical:false,
-    //     width:PAGE_WIDTH,
-    //     height:PAGE_HEIGHT,
-    //     data:{...new Array(6).keys()},
-    //     renderItem:({ index, item }) => (
-    //         <Item
-    //             key={index}
-    //             img={item}
-    //         />
-    //     ),
-    //     windowSize:5,
-    //     customAnimation:animationStyle,
-    // }
-
-    return (
-        <View>
-            {/* <Text>
-                Explore Page test update
-            </Text> */}
-            <Carousel
-                loop={false}
-                style={{
-                    width: PAGE_WIDTH,
-                    height: PAGE_HEIGHT,
-                    justifyContent: "center",
-                    alignItems: "center",
-                    backgroundColor: "white",
-                }}
-                defaultIndex={0}
-                vertical={false}
-                width={PAGE_WIDTH}
-                height={PAGE_HEIGHT}
-                // data={[...new Array(dataLength).keys()]}
-                data={tempRecipes}
-                // onConfigurePanGesture={onConfigurePanGesture}
-                // fixedDirection={fixedDirection}
-                // renderItem={({ index, item }) => (
-                //     <Item
-                //         key={index}
-                //         img={item.recipeImage}
-                //     />
-                // )}
-                customAnimation={animationStyle}
-                renderItem={({ index, item }) => (
-                    <View style={styles.renderItem}>
-                        <View style={{flexDirection:'column'}}>
-                            <Text style={styles.recipeTitleText}>
-                                {item.recipeTitle}
-                            </Text>
-                              {/* <Text>{item.description}</Text> */}
-                        </View>
-                    </View>
-                )}
-                windowSize={5}
-            />
-            {/* <Button title="fetch recipes list" onPress={fetchRecipeList}/> */}
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                <TBButton style={{ width: '25%' }} title="X" />
-                <TBButton style={{ width: '30%' }} title="Full Recipe"/>
-                <TBButton style={{ width: '25%' }} title="Save" />
-            </View>
-        </View>
-    );
-}
-
-const Item: React.FC<{ img: ImageSourcePropType }> = ({ img }) => {
-    const width = window.width * 0.9;
-    const height = window.height * 0.7;
-
-    return (
-        <Animated.View entering={FadeInDown.duration(300)} style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-            <View
-                style={{
-                    width,
-                    height,
-                    borderWidth: 1,
-                    borderColor: "black",
-                    borderRadius: 20,
-                    justifyContent: "center",
-                    alignItems: "center",
-                    backgroundColor: "white",
-                    elevation: 20,
-                }}
-            >
-                <Image source={img} style={{
-                    width,
-                    height,
-                    borderRadius: 20,
-                }} />
-            </View>
-        </Animated.View>
-    );
+  return (
+    <View style={styles.wrapper}>
+      <View style={{ flex: 1, height: PAGE_HEIGHT }}>
+        {recipes?.map((item, index) => {
+          return (
+            (
+              <View
+                key={index}
+                style={styles.cardContainer}
+                pointerEvents="box-none"
+              >
+                <TinderCard
+                  cardHeight={0.75 * PAGE_HEIGHT}
+                  cardWidth={0.95 * PAGE_WIDTH}
+                  cardStyle={styles.card}
+                  // disableTopSwipe={true}
+                  onSwipedRight={() => {
+                    saveRecipe(item.id, username);
+                  }}
+                  onSwipedLeft={() => {
+                   rejectRecipe(item.id);
+                  }}
+                  onSwipedBottom={() => {
+                    Alert.alert("Swiped Bottom");
+                  }}
+                  onSwipedTop={() =>{
+                    Alert.alert("Swiped Top");
+                  }}
+                >
+                  <View>
+                    <ImageBackground
+                      source={{ uri: item.recipeImage }}
+                      style={styles.image}
+                      imageStyle={{ borderRadius: 25 }}
+                    >
+                      <View style={{ height: 0.6 * PAGE_HEIGHT }}></View>
+                      <View style={{ flex: 1 }}>
+                        <LinearGradient
+                          colors={["rgba(0,0,0,0)", "rgba(0,0,0,0.49)"]}
+                          start={{ x: 0, y: 0 }}
+                          end={{ x: 0, y: 1 }}
+                          style={{ flex: 1, borderRadius: 25 }}
+                        >
+                          <Text style={styles.recipeTitleText}>
+                            {item.recipeTitle}
+                          </Text>
+                          <Text style={styles.recipeDescription}>
+                            {item.description}
+                          </Text>
+                        </LinearGradient>
+                      </View>
+                    </ImageBackground>
+                  </View>
+                </TinderCard>
+              </View>
+            ) || "test this?"
+          );
+        })}
+      </View>
+      {/* <Button title="fetch recipes list" onPress={fetchRecipeList}/> */}
+      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <TBButton style={{ width: "25%" }} title="X" />
+        <TBButton
+          style={{ width: "30%" }}
+          title="Full Recipe"
+          onPress={fetchRecipeList}
+        />
+        <TBButton style={{ width: "25%" }} title="Save" />
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    renderItem: {
-        flex: 1,
-        borderWidth: 1,
-        margin: 10,
-        padding: 10,
-        borderRadius: 20,
-        backgroundColor: 'cornflowerblue',
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
-        alignItems: 'flex-end'
-    },
-    recipeTitleText: {
-        textAlign: 'center',
-        fontSize: 25,
-        color: 'white'
-    },
+  recipeTitleText: {
+    // textAlign: "center",
+    fontSize: 25,
+    color: "white",
+    paddingLeft: 15,
+  },
+  recipeDescription: {
+    fontSize: 15,
+    color: "white",
+    paddingLeft: 15,
+    paddingTop: 5,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 25,
+  },
+  card: {
+    borderRadius: 25,
+  },
+  cardContainer: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "center",
+    alignItems: "center",
+    // paddingBottom: 10,
+  },
+  wrapper: {
+    flex: 1,
+  },
 });
-
 
 export default RecommenderPage;
