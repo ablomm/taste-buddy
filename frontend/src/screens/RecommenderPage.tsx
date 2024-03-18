90a2f9e885d4fbe750370311cad76d0cecebf01d
Ivy <iivy.dding@gmail.com>
2024-03-18 15:28:29 -0400

    Display default card when recipes are loading
    Also changed the logic to directly modify recipes list instead of keeping a count so that it is more consistent

--- frontend/src/screens/RecommenderPage.tsx
+++ frontend/src/screens/RecommenderPage.tsx
@@ -6,8 +6,12 @@ import { TinderCard } from "rn-tinder-card";
 import { LinearGradient } from "expo-linear-gradient";
 import { UserContext } from "../providers/UserProvider";
 import { Recipe } from "../interfaces/RecipeInterface";
-import { addRecipeToUserSaved, addUserRejectedRecipe } from "../functions/HTTPRequests";
-
+import {
+  addRecipeToUserSaved,
+  addUserRejectedRecipe,
+} from "../functions/HTTPRequests";
+import { LoadingContext } from "../providers/LoadingProvider";
+// import { RecipeTab}
 
 const fetchRecipeBatch = async (num: number) => {
   try {
@@ -45,7 +49,7 @@ const fetchRecommendedRecipes = async (userID: number) => {
         },
         credentials: "include",
         body: JSON.stringify({
-          userID: userID
+          userID: userID,
         }),
       }
     );
@@ -57,6 +61,7 @@ const fetchRecommendedRecipes = async (userID: number) => {
 };
 
 const RecommenderPage = () => {
+  const loadingContext = React.useContext(LoadingContext) as any;
   const userContext = React.useContext(UserContext) as any;
   const username = userContext.state.username;
   const userID = userContext.state.userId;
@@ -67,33 +72,46 @@ const RecommenderPage = () => {
   const [batchNum, setBatchNum] = useState(0); // how many batches of recipes have you gone through (each batch is an http call)
   const [loading, setLoading] = useState(false); //to display loading signal
   const [recipesLeft, setRecipesLeft] = useState(0); //for how many recipes have been shown to determine refresh
- 
+
   // call this whenever you need to add more recipes to the end of the list
   const loadNextBatch = async () => {
+    // loadingContext.enable();
     console.log(`loading next batch, num = ${batchNum}`);
-    let newRecipes = (await fetchRecipeBatch(batchNum)) as Recipe[];
-    let recommend = (await fetchRecommendedRecipes(userID))
-    setRecipes([...recipes, ...recommend]);
+    console.log("rec length", recipes.length);
+    // console.log("rec", recipes);
+    // let newRecipes = (await fetchRecipeBatch(batchNum)) as Recipe[];
+    let recommendedRecipes = (await fetchRecommendedRecipes(userID)) as Recipe[];
+    // let recommendedRecipes = [];
+    setRecipes([...recipes, ...recommendedRecipes]);
     setBatchNum(batchNum + 1);
-    setRecipesLeft(recipesLeft + newRecipes.length - 1);
+    setRecipesLeft(recipesLeft + recommendedRecipes.length - 1);
+    // loadingContext.disable();
   };
 
   const rejectRecipe = async (recipeID: number) => {
-    console.log(userID);
     addUserRejectedRecipe(recipeID, userID);
     setRecipesLeft(recipesLeft - 1);
-    checkRecipeList();
+    checkRecipeList(recipeID);
   };
 
   const saveRecipe = async (recipeID: number) => {
     setRecipesLeft(recipesLeft - 1);
     addRecipeToUserSaved(recipeID, username);
-    checkRecipeList();
+    checkRecipeList(recipeID);
   };
 
-  const checkRecipeList = async () => {
-    console.log(`recipes left: ${recipesLeft}`);
+  const showFullRecipe = async (recipe: Recipe) => {
+    //   navigation.navigate('RecipePage', {
+    //     recipe: item
+    // })
+    console.log("button press ", recipe.recipeTitle);
+  };
 
+  const checkRecipeList = async (recipeID: number) => {
+    console.log(`recipes left: ${recipesLeft}`);
+    // filter out recipe in recipe list so that list length can be tracked
+    setRecipes(recipes.filter((recipe) => recipe.id !== recipeID))
+    console.log("how do i know rec length", recipes.length);
     if (recipesLeft == 1) {
       loadNextBatch();
     }
@@ -106,113 +124,173 @@ const RecommenderPage = () => {
   return (
     <View style={styles.wrapper}>
       <View style={{ flex: 1, height: PAGE_HEIGHT }}>
-        {recipes.map((item, index) => {
-          return (
-            (
+        {recipes && recipes.length > 0 ? (
+          recipes.map((item, index) => {
+            return (
               <View
                 key={index}
                 style={[styles.cardContainer, { zIndex: -index }]} // need zindex because by default the last item in the list was displaying first
                 pointerEvents="box-none"
               >
                 <TinderCard
-                  cardHeight={0.75 * PAGE_HEIGHT}
+                  cardHeight={0.82 * PAGE_HEIGHT}
                   cardWidth={0.95 * PAGE_WIDTH}
                   cardStyle={styles.card}
-                  // disableTopSwipe={true}
+                  disableTopSwipe={true}
+                  disableBottomSwipe={true}
                   onSwipedRight={() => {
                     saveRecipe(item.id);
                   }}
                   onSwipedLeft={() => {
                     rejectRecipe(item.id);
                   }}
-                  onSwipedBottom={() => {
-                    Alert.alert("Swiped Bottom");
-                  }}
+
                   onSwipedTop={() => {
-                    Alert.alert("Swiped Top");
+                    showFullRecipe(item);
                   }}
                 >
                   <View>
                     <ImageBackground
                       source={{ uri: item.recipeImage }}
                       style={styles.image}
-                      imageStyle={{ borderRadius: 25 }}
+                      imageStyle={{ borderRadius: 20 }}
                     >
-                      <View style={{ height: 0.6 * PAGE_HEIGHT }}></View>
                       <View style={{ flex: 1 }}>
                         <LinearGradient
-                          colors={["rgba(0,0,0,0)", "rgba(0,0,0,0.49)"]}
+                          colors={["rgba(0,0,0,0)", "rgba(0,0,0,1)"]}
                           start={{ x: 0, y: 0 }}
-                          end={{ x: 0, y: 1 }}
-                          style={{ flex: 1, borderRadius: 25 }}
+                          end={{ x: 0, y: 2 }}
+                          style={{ flex: 1, borderRadius: 20 }}
                         >
-                          <Text style={styles.recipeTitleText}>
-                            {item.recipeTitle}
-                          </Text>
-                          <Text style={styles.recipeDescription}>
-                            {item.description}
-                          </Text>
+                          <View
+                            style={{
+                              height: 0.65 * PAGE_HEIGHT,
+                              flexDirection: "column-reverse",
+                            }}
+                          >
+                            <Text
+                              numberOfLines={3}
+                              style={styles.recipeTitleText}
+                            >
+                              {item.recipeTitle}
+                            </Text>
+                          </View>
+                          <View style={{ height: 0.11 * PAGE_HEIGHT }}>
+                            <Text
+                              numberOfLines={5}
+                              style={styles.recipeDescription}
+                            >
+                              {item.description}
+                            </Text>
+                          </View>
+                          <View
+                            style={{
+                              height: 0.06 * PAGE_HEIGHT,
+                              paddingHorizontal: "3.5%",
+                              justifyContent: "space-evenly",
+                            }}
+                          >
+                            <TBButton
+                              title="See Full Recipe"
+                              onPress={() => {
+                                showFullRecipe(item);
+                              }}
+                              style={styles.fullRecipeButton}
+                              textColor={{ color: "white" }}
+                            ></TBButton>
+                          </View>
                         </LinearGradient>
                       </View>
                     </ImageBackground>
                   </View>
                 </TinderCard>
               </View>
-            ) || "test this?"
-          );
-        })}
-      </View>
-      {/* <Button title="fetch recipes list" onPress={fetchRecipeList}/> */}
-      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-        <TBButton
-          style={{ width: "25%" }}
-          title="X"
-          onPress={() => loadNextBatch()}
-        />
-        <TBButton
-          style={{ width: "30%" }}
-          title="Full Recipe"
-          onPress={() => loadNextBatch()}
-        />
-        <TBButton
-          style={{ width: "25%" }}
-          title="Save"
-          onPress={() => saveRecipe(2)}
-        />
+            );
+          })
+        ) : (
+          <View style={[styles.cardContainer]} // need zindex because by default the last item in the list was displaying first
+          pointerEvents="box-none">
+            <TinderCard
+              cardHeight={0.82 * PAGE_HEIGHT}
+              cardWidth={0.95 * PAGE_WIDTH}
+              cardStyle={styles.defaultCard}
+              disableBottomSwipe={true}
+              disableLeftSwipe={true}
+              disableRightSwipe={true}
+              disableTopSwipe={true}
+            >
+              <View
+                style={{
+                  flex: 1,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Text style={styles.defaultText}>
+                  One moment! We're finding you some new recommendations...
+                </Text>
+              </View>
+            </TinderCard>
+          </View>
+        )}
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  fullRecipeButton: {
+    width: "40%",
+    height: 30,
+    borderColor: "white",
+    margin: 5,
+    marginLeft: "auto",
+    marginRight: 0,
+    marginTop:0,
+  },
   recipeTitleText: {
-    // textAlign: "center",
     fontSize: 25,
     color: "white",
-    paddingLeft: 15,
+    paddingHorizontal: "4.5%",
+    textAlign: "justify",
+  },
+  defaultText: {
+    fontSize: 25,
+    color: "black",
+    fontWeight: "500",
+    textAlign: "center",
   },
   recipeDescription: {
     fontSize: 15,
     color: "white",
-    paddingLeft: 15,
-    paddingTop: 5,
+    paddingHorizontal: "4.5%",
+    paddingTop: 2,
+    textAlign: "justify",
   },
   image: {
     width: "100%",
     height: "100%",
-    borderRadius: 25,
+    borderRadius: 20,
   },
   card: {
-    borderRadius: 25,
+    borderRadius: 20,
+    overflow: "hidden",
+  },
+  defaultCard: {
+    borderRadius: 20,
+    borderColor: "black",
+    borderWidth: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
   cardContainer: {
     ...StyleSheet.absoluteFillObject,
     justifyContent: "center",
     alignItems: "center",
-    // paddingBottom: 10,
   },
   wrapper: {
     flex: 1,
+    overflow: "hidden",
   },
 });
 
