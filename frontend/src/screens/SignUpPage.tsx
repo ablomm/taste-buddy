6081f15a7749b450aea1bd114377188c5be57429
talmoha@uwo.ca <talmoha@uwo.ca>
2024-03-20 00:49:07 -0400

    added logo+name header on all pages except loginorsignup page

--- frontend/src/screens/SignUpPage.tsx
+++ frontend/src/screens/SignUpPage.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { View, Text, TextInput, StyleSheet, Button, Pressable } from "react-native";
+import { View, Text, TextInput, StyleSheet, Button, Pressable, Image} from "react-native";
 import Header from '../components/header/Header';
 import SignUpForm from '../components/loginSignupPageComponents/SignUpForm';
 
 const SignUpPage = () => {
     return (
         <View style={styles.container}>
+            <View style={{ alignItems: 'center', flexDirection: 'row', justifyContent: 'center'}}>
+                <Image
+                    source={require("../../assets/logo/Logo.png")}
+                    style={{ height: 42.7, width: 135.1, marginRight: 10}}
+                />
+                <Text >t a s t e b u d d y</Text>
+            </View>
             <Header title = "Sign Up" />
             <SignUpForm />
         </View>
