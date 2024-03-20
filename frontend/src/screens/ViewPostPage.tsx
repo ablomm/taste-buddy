6c33ae871583720135f39c565ef1e449790ce0f0
ablomm <71096624+ablomm@users.noreply.github.com>
2024-03-20 12:21:07 -0400

    make image bigger

--- frontend/src/screens/ViewPostPage.tsx
+++ frontend/src/screens/ViewPostPage.tsx
@@ -121,7 +121,7 @@ const styles = StyleSheet.create({
   },
   image: {
     width: "100%",
-    height: 300,
+    height: 350,
     alignSelf: "center",
     marginBottom: 10,
     borderRadius: 10,
