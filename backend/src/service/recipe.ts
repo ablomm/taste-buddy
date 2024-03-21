db469cf007dab3485e617ce6c9db4f29a0fa4efb
HerbertShin <hshin63@uwo.ca>
2024-03-21 15:07:02 -0400

    Cleanup in recipe service file

--- backend/src/service/recipe.ts
+++ backend/src/service/recipe.ts
@@ -313,9 +313,6 @@ export async function updateIngredients(
         },
     });
 
-    console.log("Retrieved old ingredients: " + JSON.stringify(ingredients));
-    console.log("Retrieved new ingredients: " + JSON.stringify(newIngredients));
-
     // Compare new ingredients to old ingredients
     let ingredientsToAdd: Ingredient[] = [];
     let ingredientsToRemove: number[] = [];
@@ -340,12 +337,6 @@ export async function updateIngredients(
                         },
                         data: newIngredient,
                     });
-                    console.log(
-                        "Updated " +
-                        oldIngredient.id +
-                        " with " +
-                        JSON.stringify(newIngredient)
-                    );
                 }
                 break;
             }
@@ -384,8 +375,6 @@ export async function updateIngredients(
                 },
             },
         });
-
-        console.log("Removed Ingredient IDs: " + ingredientsToRemove);
     }
 
     // Add new ingredients
@@ -393,8 +382,6 @@ export async function updateIngredients(
         await prisma.recipeIngredients.createMany({
             data: processIngredients(recipeId, ingredientsToAdd),
         });
-
-        console.log("Added: " + JSON.stringify(ingredientsToAdd));
     }
 
     console.log("Ingredient update completed ...");
@@ -420,9 +407,6 @@ export async function updateInstructions(
                 recipeID: recipeId,
             },
         });
-        // Compare existing to new instructions
-        console.log("Old instructions: " + JSON.stringify(instructions));
-        console.log("New instructions: " + JSON.stringify(newInstructions));
 
         let containSameInstructions: boolean = true;
 
@@ -454,7 +438,6 @@ export async function updateInstructions(
 
             // insert new instructions
             await createInstructions(recipeId, newInstructions);
-            console.log("New instructions added ...");
         }
 
         console.log("Instruction update complete ...");
@@ -464,10 +447,6 @@ export async function updateInstructions(
             error
         );
     }
-}
-
-export async function updateRecipeTags(recipeId: number, newTags: []) {
-    // TODO: Implement once tags are fully implemented
 }
 
 /**
