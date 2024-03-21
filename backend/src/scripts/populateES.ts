bfb20a0d6c8ddcde91835273fee202dcd973dd10
ablomm <71096624+ablomm@users.noreply.github.com>
2024-03-21 13:34:30 -0400

    fix bug with script

--- backend/src/scripts/populateES.ts
+++ backend/src/scripts/populateES.ts
@@ -21,21 +21,8 @@ const populateESRecipes = async () => {
     });
 
     for (let recipe of recipes) {
-        const ingredientsObj = processIngredients(recipe.id, recipe.ingredients);
-        const instructionsObj = processInstructions(recipe.id, recipe.ingredients);
-        const processedTags = processTags(recipe.tags);
-
-
-        // Assemble elastic search recipe object
-        const elasticSearchRecipe: any = {
-            ...recipe,
-            ingredients: ingredientsObj,
-            instructions: instructionsObj,
-            tags: processedTags
-        }
-
         // Store in elastic search db
-        await storeRecipe(elasticSearchRecipe, recipe.id);
+        await storeRecipe(recipe, recipe.id);
     }
 }
 
