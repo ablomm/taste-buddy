6d319b4aa5856efa0697d8461379596ad8d8695b
ablomm <71096624+ablomm@users.noreply.github.com>
2024-03-21 13:08:00 -0400

    populate posts in elasticsearchdb;

--- backend/src/scripts/populateES.ts
+++ backend/src/scripts/populateES.ts
@@ -1,10 +1,17 @@
 import { PrismaClient } from "@prisma/client"
 import { processIngredients, processInstructions, processTags } from "../service/recipe";
-import { storeRecipe } from "../service/search";
+import { storePost, storeRecipe } from "../service/search";
 const prisma = new PrismaClient()
 
-
 const populateES = async () => {
+    await populateESRecipes();
+    await populateESPosts();
+
+    process.exit(0);
+
+}
+
+const populateESRecipes = async () => {
     let recipes = await prisma.recipe.findMany({
         include: {
             ingredients: true,
@@ -18,7 +25,7 @@ const populateES = async () => {
         const instructionsObj = processInstructions(recipe.id, recipe.ingredients);
         const processedTags = processTags(recipe.tags);
 
-    
+
         // Assemble elastic search recipe object
         const elasticSearchRecipe: any = {
             ...recipe,
@@ -26,12 +33,24 @@ const populateES = async () => {
             instructions: instructionsObj,
             tags: processedTags
         }
-    
+
         // Store in elastic search db
         await storeRecipe(elasticSearchRecipe, recipe.id);
     }
-
-   
 }
+
+const populateESPosts = async () => {
+    let posts = await prisma.posts.findMany({
+        include: {
+            postTags: true,
+        }
+    });
+
+    for (let post of posts) {
+        // Store in elastic search db
+        await storePost(post.author, post.description, post.postTags, post.image, post.recipeURL, post.id);
+    }
+}
+
 
 export default populateES;
