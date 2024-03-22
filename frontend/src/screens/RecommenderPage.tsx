fddabe10e0ec8d36f221284fa750bf9a2e1efd6b
ablomm <71096624+ablomm@users.noreply.github.com>
2024-03-21 22:53:25 -0400

    style fixes recommendation engine

--- frontend/src/screens/RecommenderPage.tsx
+++ frontend/src/screens/RecommenderPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, ImageBackground, Alert } from "react-native";
+import { View, Text, StyleSheet, ImageBackground, Alert, ActivityIndicator } from "react-native";
 import { window } from "../constants";
 import TBButton from "../components/TBButton";
 import { TinderCard } from "rn-tinder-card";
@@ -20,6 +20,8 @@ interface InstructionStep {
 interface FormattedInstructions {
   list: InstructionStep[];
 }
+
+let isLoadingNextBatch = false;
 
 const fetchRecommendedRecipes = async (userID: number) => {
   try {
@@ -52,37 +54,38 @@ const RecommenderPage = ({ navigation }) => {
   const PAGE_WIDTH = window.width;
   const PAGE_HEIGHT = window.height;
 
-  const [recipes, setRecipes] = useState<Recipe[]>([]); //recipes to display to user
+  const [recipes, setRecipes] = useState<any[]>([]); //recipes to display to user
   const [batchNum, setBatchNum] = useState(0); // how many batches of recipes have you gone through (each batch is an http call)
   const [recipesLeft, setRecipesLeft] = useState(0); //for how many recipes have been shown to determine refresh
+
+
 
   // call this whenever you need to add more recipes to the end of the list
   const loadNextBatch = async () => {
     console.log("loading next batch")
-  let recommendedRecipes = (await fetchRecommendedRecipes(
+    let recommendedRecipes = (await fetchRecommendedRecipes(
       userID
     )) as Recipe[];
     setRecipes([...recipes, ...recommendedRecipes]);
     setBatchNum(batchNum + 1);
     setRecipesLeft(currentRecipesLeft => currentRecipesLeft + recommendedRecipes.length - 1);
+    isLoadingNextBatch = false;
   };
 
   const rejectRecipe = async (recipeID: number) => {
     // console.log("rejecting: ", recipeID);
     // filter out recipe in recipe list so that list length can be tracked
-    setRecipes(currentRecipes => currentRecipes.filter(recipe => recipe.id !== recipeID));
     addUserRejectedRecipe(recipeID, userID);
     setRecipesLeft(currentRecipesLeft => currentRecipesLeft - 1);
-    checkRecipeList(recipeID);
+    checkRecipeList();
   };
 
   const saveRecipe = async (recipeID: number) => {
     // console.log("saving: ", recipeID);
     // filter out recipe in recipe list so that list length can be tracked
-    setRecipes(currentRecipes => currentRecipes.filter(recipe => recipe.id !== recipeID));
     setRecipesLeft(currentRecipesLeft => currentRecipesLeft - 1);
     addRecipeToUserSaved(recipeID, username);
-    checkRecipeList(recipeID);
+    checkRecipeList();
   };
 
   const showFullRecipe = async (recipe: Recipe) => {
@@ -95,36 +98,38 @@ const RecommenderPage = ({ navigation }) => {
     let formattedInstructions: FormattedInstructions = {
       list: []
     };
-  
+
     recipeInstructions.forEach((item, index) => {
       formattedInstructions.list.push({
         step: index + 1,
         instruction: item
       });
     });
-  
+
     return formattedInstructions;
   }
 
-  const checkRecipeList = async (recipeID: number) => {
-    if (recipesLeft == 1) {
+  const checkRecipeList = async () => {
+    console.log(recipesLeft)
+    if (!isLoadingNextBatch && recipesLeft <= 20) {
+      isLoadingNextBatch = true;
       loadNextBatch();
     }
   };
 
   useEffect(() => {
-    loadNextBatch();
+    checkRecipeList();
   }, []);
 
   return (
     <View style={styles.wrapper}>
       <View style={{ flex: 1, height: PAGE_HEIGHT }}>
-        {recipes && recipes.length > 0 ? (
+        {recipesLeft > 0 ? (
           recipes.map((item, index) => {
             return (
               <View
                 key={index}
-                style={[styles.cardContainer]} 
+                style={[styles.cardContainer, {zIndex: -index}]}
                 pointerEvents="box-none"
               >
                 <TinderCard
@@ -204,15 +209,7 @@ const RecommenderPage = ({ navigation }) => {
             style={[styles.cardContainer]} // need zindex because by default the last item in the list was displaying first
             pointerEvents="box-none"
           >
-            <TinderCard
-              cardHeight={0.80 * PAGE_HEIGHT}
-              cardWidth={0.95 * PAGE_WIDTH}
-              cardStyle={styles.defaultCard}
-              disableBottomSwipe={true}
-              disableLeftSwipe={true}
-              disableRightSwipe={true}
-              disableTopSwipe={true}
-            >
+          
               <View
                 style={{
                   flex: 1,
@@ -223,8 +220,8 @@ const RecommenderPage = ({ navigation }) => {
                 <Text style={styles.defaultText}>
                   One moment! We're finding you some recommendations...
                 </Text>
+                <ActivityIndicator color={"#000"} />
               </View>
-            </TinderCard>
           </View>
         )}
       </View>
